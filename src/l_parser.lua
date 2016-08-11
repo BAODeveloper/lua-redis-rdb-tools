9fdd7f28dfc371ef0f7345632bb00a9e27bdd95c
sagrada1788 <644696751@qq.com>
2016-08-11 11:34:41 +0800

    support all data type

--- src/l_parser.lua
+++ src/l_parser.lua
@@ -1,5 +1,7 @@
+local StringIO = require 'StringIO'
 local byte, sub, char = string.byte, string.sub, string.char
 local insert, concat = table.insert, table.concat
+local tostring, tonumber = tostring, tonumber
 local open = io.open
 local bit = require 'bit'
 local bit_band, bit_bor, bit_rshift, bit_lshift = bit.band, bit.bor, bit.rshift, bit.lshift
@@ -29,6 +31,12 @@ local REDIS_RDB_ENC_INT8 = 0
 local REDIS_RDB_ENC_INT16 = 1
 local REDIS_RDB_ENC_INT32 = 2
 local REDIS_RDB_ENC_LZF = 3
+
+local VALUE_TYPE_STRING = 'string'
+local VALUE_TYPE_LIST = 'list'
+local VALUE_TYPE_SET = 'set'
+local VALUE_TYPE_ZSET = 'zset'
+local VALUE_TYPE_HASH = 'hash'
 
 local function get_number(data)
     local v = 0
@@ -189,7 +197,7 @@ local function read_length(f)
     return length, concat(bts, '')
 end
 
-local function read_string(f, is_key)
+local function read_string(f)
     local length, is_encoded, bts = read_length_with_encoding(f)
     local val, orig_val = nil, nil
     if is_encoded then
@@ -206,9 +214,7 @@ local function read_string(f, is_key)
             local clen, orig_clen = read_length(f)
             local l, orig_l = read_length(f)
             orig_val = f:read(clen)
-            if is_key then
-                val = lzf_decompress(orig_val, l)
-            end
+            val = lzf_decompress(orig_val, l)
             insert(bts, orig_clen)
             insert(bts, orig_l)
             insert(bts, orig_val)
@@ -228,39 +234,193 @@ local _callback = {
     end
 }
 
--- TODO
--- add callback
+local function skip(f, free)
+    free = tonumber(free)
+    if free then
+        f:read(free)
+    end
+end
+
+local function read_ziplist_entry(f)
+    local length, value = 0, nil
+    local prev_length = read_unsigned_char(f)
+    if prev_length == 254 then
+        prev_length = read_unsigned_int(f)
+    end
+    
+    local entry_header = read_unsigned_char(f)
+    if bit_rshift(entry_header, 6) == 0 then
+        length = bit_band(entry_header, 0x3f)
+        value = f:read(length)
+    elseif bit_rshift(entry_header, 6) == 1 then
+        length = bit_band(entry_header, 0x3f)
+        length = bit_lshift(length, 8)
+        local tmp = read_unsigned_char(f)
+        length = bit_bor(length, tmp)
+        value = f:read(length)
+    elseif bit_rshift(entry_header, 6) == 2 then
+        length = read_bid_endian_unsigned_int(f)
+        value = f:read(length)
+    elseif bit_rshift(entry_header, 4) == 12 then
+        value = read_signed_short(f)
+    elseif bit_rshift(entry_header, 4) == 13 then
+        value = read_signed_int(f)
+    elseif bit_rshift(entry_header, 4) == 14 then
+        value = read_signed_long(f)
+    elseif entry_header == 240 then
+        value = read_24bit_signed_number(f)
+    elseif entry_header == 254 then
+        value = read_signed_char(f)
+    elseif entry_header >= 241 and entry_header <= 253 then
+        value = entry_header - 241
+    else
+        -- error
+    end
+    return value
+end
+
+local function read_zipmap_next_length(f)
+    local num = read_unsigned_char(f)
+    if num < 254 then
+        return num
+    elseif num == 254 then
+        num = read_unsigned_int(f)
+        return num
+    else
+        return nil
+    end
+end
+
+local function read_zipmap(buff)
+    local num_entries = read_unsigned_char(buff)
+    local info = {}
+    while true do
+        local next_length = read_zipmap_next_length(buff)
+        if not next_length then
+            break
+        end
+
+        local field = buff:read(next_length)
+        next_length = read_zipmap_next_length(buff)
+        if not next_length then
+            break
+        end
+
+        local free = read_unsigned_char(buff)
+        local value = buff:read(next_length)
+        value = tonumber(value) or value
+
+        info[tostring(field)] = value
+        skip(buff, free)
+    end
+    return info
+end
+
+local function read_ziplist(buff)
+    local zlbytes = read_unsigned_int(buff)
+    local tail_offset = read_unsigned_int(buff)
+    local num_entries = read_unsigned_short(buff)
+
+    local info = {}
+    for i=1,num_entries,1 do
+        local val = read_ziplist_entry(buff)
+        info[i] = val
+    end
+    local zlist_end = read_unsigned_char(buff)
+    return info
+end
+
+local function read_intset(buff)
+    local encoding = read_unsigned_int(buff)
+    local num_entries = read_unsigned_short(buff)
+
+    local info = {}
+    for i=1,num_entries,1 do
+        local entry = nil
+        if encoding == 8 then
+            entry = read_unsigned_long(buff)
+        elseif encoding == 4 then
+            entry = read_unsigned_int(buff)
+        elseif encoding == 2 then
+            entry = read_unsigned_short(buff)
+        else
+            --error
+        end
+        insert(info, entry)
+    end
+    return info
+end
+
+local function read_zset_from_ziplist(buff)
+    local zlbytes = read_unsigned_int(buff)
+    local tail_offset = read_unsigned_int(buff)
+    local num_entries = read_unsigned_short(buff)
+
+    num_entries = num_entries / 2
+
+    local info = {}
+    for i=1,num_entries,1 do
+        local member = read_ziplist_entry(buff)
+        local score = read_ziplist_entry(buff)
+        info[tostring(member)] = score
+    end
+    local zlist_end = read_unsigned_char(buff)
+    return info
+end
+
+local function read_hash_from_ziplist(buff)
+    local zlbytes = read_unsigned_int(buff)
+    local tail_offset = read_unsigned_int(buff)
+    local num_entries = read_unsigned_short(buff)
+
+    num_entries = num_entries / 2
+
+    local info = {}
+    for i=1,num_entries,1 do
+        local field = read_ziplist_entry(buff)
+        local value = read_ziplist_entry(buff)
+        info[tostring(field)] = value
+    end
+    local zlist_end = read_unsigned_char(buff)
+    return info
+end
+
 local function read_object(f, enc_type)
-    local val, orig = nil, nil
+    local val, orig, val_type = nil, nil, nil
 
     if enc_type == REDIS_RDB_TYPE_STRING then
         val, orig_val = read_string(f)
-        return val, orig_val
+        return val, orig_val, VALUE_TYPE_STRING
 
     elseif enc_type == REDIS_RDB_TYPE_LIST then
         local _orig = {}
         local length, orig_length = read_length(f)
         insert(_orig, orig_length)
+        local info = {}
         for i=1,length,1 do
             local value, orig_value = read_string(f)
             insert(_orig, orig_value)
+            insert(info, value)
         end
-        return nil, concat(_orig, '')
+        return info, concat(_orig, ''), VALUE_TYPE_LIST
 
     elseif enc_type == REDIS_RDB_TYPE_SET then
         local _orig = {}
         local length, orig_length = read_length(f)
         insert(_orig, orig_length)
+        local info = {}
         for i=1,length,1 do
             local value, orig_value = read_string(f)
             insert(_orig, orig_value)
+            insert(info, value)
         end
-        return nil, concat(_orig, '')
+        return info, concat(_orig, ''), VALUE_TYPE_SET
 
     elseif enc_type == REDIS_RDB_TYPE_ZSET then
         local _orig = {}
         local length, orig_length = read_length(f)
         insert(_orig, orig_length)
+        local info = {}
         for i=1,length,1 do
             local value, orig_value = read_string(f)
             local dbl_length, orig_dbl_length = read_unsigned_char(f)
@@ -268,43 +428,51 @@ local function read_object(f, enc_type)
             insert(_orig, orig_value)
             insert(_orig, orig_dbl_length)
             insert(_orig, score)
+            info[tostring(value)] = score
         end
-        return nil, concat(_orig, '')
+        return info, concat(_orig, ''), VALUE_TYPE_ZSET
 
     elseif enc_type == REDIS_RDB_TYPE_HASH then
         local _orig = {}
         local length, orig_length = read_length(f)
         insert(_orig, orig_length)
+        local info = {}
         for i=1,length,1 do
             local field, orig_field = read_string(f)
             local value, orig_value = read_string(f)
             insert(_orig, orig_field)
             insert(_orig, orig_value)
+            info[tostring(field)] = value
         end
-        return nil, concat(_orig, '')
+        return info, concat(_orig, ''), VALUE_TYPE_HASH
 
     elseif enc_type == REDIS_RDB_TYPE_HASH_ZIPMAP then
         val, orig_val = read_string(f)
-        return val, orig_val
+        local info = read_zipmap(StringIO:new(val))
+        return info, orig_val, VALUE_TYPE_HASH
 
     elseif enc_type == REDIS_RDB_TYPE_LIST_ZIPLIST then
         val, orig_val = read_string(f)
-        return val, orig_val
+        local info = read_ziplist(StringIO:new(val))
+        return info, orig_val, VALUE_TYPE_LIST
 
     elseif enc_type == REDIS_RDB_TYPE_SET_INTSET then
         val, orig_val = read_string(f)
-        return val, orig_val
+        local info = read_intset(StringIO:new(val))
+        return info, orig_val, VALUE_TYPE_SET
 
     elseif enc_type == REDIS_RDB_TYPE_ZSET_ZIPLIST then
         val, orig_val = read_string(f)
-        return val, orig_val
+        local info = read_zset_from_ziplist(StringIO:new(val))
+        return info, orig_val, VALUE_TYPE_ZSET
 
     elseif enc_type == REDIS_RDB_TYPE_HASH_ZIPLIST then
         val, orig_val = read_string(f)
-        return val, orig_val
+        local info = read_hash_from_ziplist(StringIO:new(val))
+        return info, orig_val, info, VALUE_TYPE_HASH
     end
 
-    return nil, nil
+    return nil, nil, nil
 end
 --------------------------------------------
 
@@ -319,14 +487,14 @@ function l_parser:parse(filename, callback)
     local magic_string = f:read(5)
     local rdb_version = f:read(4)
 
-    local db_number = 0
+    local db_num = 0
 
     while true do
         local data_type, orig_data_type = read_unsigned_char(f)
-        local expire, orig_expiry = nil, nil
+        local expire, ms_expire, orig_expiry = nil, nil
 
         if data_type == REDIS_RDB_OPCODE_EXPIRETIME_MS then
-            expire, orig_expiry = read_unsigned_long(f)
+            ms_expire, orig_expiry = read_unsigned_long(f)
             orig_expiry = orig_data_type .. orig_expiry
             data_type, orig_data_type = read_unsigned_char(f)
         elseif data_type == REDIS_RDB_OPCODE_EXPIRETIME then
@@ -343,12 +511,24 @@ function l_parser:parse(filename, callback)
                 break
             end
 
-            local key, orig_key = read_string(f, true)
-            local value, orig_value = read_object(f, data_type)
+            local key, orig_key = read_string(f)
+            local value, orig_value, value_type = read_object(f, data_type)
 
-            -- TODO
             if orig_value then
-                callback:set(key, orig_key, orig_expiry, orig_data_type, orig_value)
+                local orig_data = {
+                    orig_key = orig_key,
+                    orig_expiry = orig_expiry,
+                    orig_data_type = orig_data_type,
+                    orig_value = orig_value,
+                }
+                local data = {
+                    key = key,
+                    expire = tonumber(expire),
+                    ms_expire = tonumber(ms_expire),
+                    value = value,
+                }
+
+                callback:set(key, value_type, orig_data, data)
             end
         end
     end
